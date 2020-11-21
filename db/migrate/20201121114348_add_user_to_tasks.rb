163543de0e76fdf8e345b8d40ae6c46005028a6f
sakutaro.hagiwara <kzkc.ggg@gmail.com>
2020-11-21 12:23:48 +0000

    tasklist login

--- db/migrate/20201121114348_add_user_to_tasks.rb
+++ db/migrate/20201121114348_add_user_to_tasks.rb
@@ -1,10 +1,10 @@
-class CreateTasks < ActiveRecord::Migration[5.2]
+class AddUserToTasks < ActiveRecord::Migration[5.2]
   def change
-    create_table :tasks do |t|
+    create_table :microposts do |t|
       t.string :content
       t.references :user, foreign_key: true
 
       t.timestamps
-    end
+  end
   end
 end
