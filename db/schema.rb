4d4c47158bde8d65df57d9cf1f805644fc8174d8
sakutaro.hagiwara <kzkc.ggg@gmail.com>
2020-11-26 14:12:01 +0000

    tasklist OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_114348) do
+ActiveRecord::Schema.define(version: 2020_11_26_132100) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
